c52e22a183c61b25612a5ecb3d6bf7a1387cd6ea
wartave <vatuta12@gmail.com>
2023-11-27 13:28:34 -0400

    update
    update now

--- pod1/index.js
+++ pod1/index.js
@@ -11,31 +11,89 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send(`
-  <h1>Busqueda de un elemento en un arreglo utilizando jumpSearch</h1>
-  <form action="/jumpSearch" method="post">
-    <input type="text" name="arr" value="" required/>
-    <input type="text" name="target" value="2" required/>
-    <button type="submit">jumpSearch</button>
-  </form>
-  <h1>Busqueda de un elemento en un arreglo utilizando linearSearch</h1>
-  <form action="/linearSearch" method="post">
-    <input type="text" name="arr" value="" required />
-    <input type="text" name="target" value="2" required/>
-    <button type="submit">linearSearch</button>
-  </form>
-  <script>
-  window.onload = function() {
-    let arrInputs = document.getElementsByName("arr");
-    if (arrInputs[0].value === "") {
-      let randomArr = Array.from({length: 5}, () => Math.floor(Math.random() * 50) + 1);
-      const sort = [...randomArr].sort((a, b) => a - b).join(", ");
-      arrInputs[0].value = sort;
-      arrInputs[1].value = randomArr.join(", ");
-    }
-  };          
-  </script>
-`);
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Search Algorithms</title>
+      <style>
+        body {
+          font-family: 'Roboto', sans-serif;
+          background-color: #f5f5f5;
+          margin: 0;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          height: 100vh;
+        }
+    
+        h1 {
+          color: #4285f4;
+        }
+    
+        form {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin-top: 20px;
+        }
+    
+        input {
+          margin: 5px;
+          padding: 10px;
+          width: 250px;
+          box-sizing: border-box;
+        }
+    
+        button {
+          background-color: #4285f4;
+          color: #ffffff;
+          border: none;
+          padding: 10px 20px;
+          cursor: pointer;
+        }
+    
+        button:hover {
+          background-color: #0f4aa4;
+        }
+      </style>
+    </head>
+    
+    <body>
+      <h1>Algoritmo de búsqueda por saltos</h1>
+      <form action="/jumpSearch" method="post">
+        <input type="text" name="arr" value="" placeholder="Enter sorted array" required />
+        <input type="text" name="target" value="2" placeholder="Enter target value" required />
+        <button type="submit">Jump Search</button>
+      </form>
+    
+      <h1>SAlgoritmo de búsqueda Lineal o Secuencial</h1>
+      <form action="/linearSearch" method="post">
+        <input type="text" name="arr" value="" placeholder="Enter array" required />
+        <input type="text" name="target" value="2" placeholder="Enter target value" required />
+        <button type="submit">Linear Search</button>
+      </form>
+    
+      <script>
+        window.onload = function () {
+          let arrInputs = document.getElementsByName("arr");
+          if (arrInputs[0].value === "") {
+            let randomArr = Array.from({ length: 5 }, () => Math.floor(Math.random() * 50) + 1);
+            const sort = [...randomArr].sort((a, b) => a - b).join(", ");
+            arrInputs[0].value = sort;
+            arrInputs[1].value = randomArr.join(", ");
+          }
+        };
+      </script>
+    </body>
+    
+    </html>
+  `);
 });
+
 
 app.post("/jumpSearch", (req, res) => {
   const { arr, target } = req.body;
--- pod2/index.js
+++ pod2/index.js
@@ -12,30 +12,88 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send(`
-          <h1>Busqueda de un elemento en un arreglo utilizando binarySearch</h1>
-          <form action="/binarySearch" method="post">
-            <input type="text" name="arr" value="" required />
-            <input type="text" name="target" value="2" required/>
-            <button type="submit">binarySearch</button>
-          </form>
-          <h1>Ordenamiento de un arreglo utilizando bubbleSort</h1>
-          <form action="/bubbleSort" method="post">
-            <input type="text" name="arr" value="" required/>
-            <button type="submit">bubbleSort</button>
-          </form>
-          <script>
-          window.onload = function() {
-            let arrInputs = document.getElementsByName("arr");
-            if (arrInputs[0].value === "") {
-              let randomArr = Array.from({length: 5}, () => Math.floor(Math.random() * 50) + 1);
-              const sort = [...randomArr].sort((a, b) => a - b).join(", ");
-              arrInputs[0].value = sort;
-              arrInputs[1].value = randomArr.join(", ");
-            }
-          };          
-          </script>
-        `);
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Search and Sort Algorithms</title>
+      <style>
+        body {
+          font-family: 'Roboto', sans-serif;
+          background-color: #f5f5f5;
+          margin: 0;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          height: 100vh;
+        }
+
+        h1 {
+          color: #4285f4;
+        }
+
+        form {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin-top: 20px;
+        }
+
+        input {
+          margin: 5px;
+          padding: 10px;
+          width: 250px;
+          box-sizing: border-box;
+        }
+
+        button {
+          background-color: #4285f4;
+          color: #ffffff;
+          border: none;
+          padding: 10px 20px;
+          cursor: pointer;
+        }
+
+        button:hover {
+          background-color: #0f4aa4;
+        }
+      </style>
+    </head>
+
+    <body>
+      <h1>Algoritmo de búsqueda binaria</h1>
+      <form action="/binarySearch" method="post">
+        <input type="text" name="arr" value="" placeholder="Enter sorted array" required />
+        <input type="text" name="target" value="2" placeholder="Enter target value" required />
+        <button type="submit">Binary Search</button>
+      </form>
+
+      <h1>Algoritmo de búsqueda por método de la burbuja</h1>
+      <form action="/bubbleSort" method="post">
+        <input type="text" name="arr" value="" placeholder="Enter array" required />
+        <button type="submit">Bubble Sort</button>
+      </form>
+
+      <script>
+        window.onload = function () {
+          let arrInputs = document.getElementsByName("arr");
+          if (arrInputs[0].value === "") {
+            let randomArr = Array.from({ length: 5 }, () => Math.floor(Math.random() * 50) + 1);
+            const sort = [...randomArr].sort((a, b) => a - b).join(", ");
+            arrInputs[0].value = sort;
+            arrInputs[1].value = randomArr.join(", ");
+          }
+        };
+      </script>
+    </body>
+
+    </html>
+  `);
 });
+
 
 app.post("/binarySearch", (req, res) => {
   const { arr, target } = req.body;
